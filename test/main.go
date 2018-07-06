f34ed2b16914c0eab0c90ea047ae53029655b793
tanqian <tanqian@cangoonline.com>
2018-07-06 15:28:21 +0800

    init

---
+++ test/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	_ "test/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func init() {
+	orm.RegisterDataBase("default", "mysql", "root:Cango@1219@tcp(10.42.0.121:3306)/test")
+}
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
+
